fa8f576d415008723e2935dae35aca2ec01ae71b
Risheit <80658104+Risheit@users.noreply.github.com>
2022-06-13 01:11:05 -0500

    Modified CMakeLists

---
+++ tests/UnitTests/EventLogTests.cpp
@@ -0,0 +1,27 @@
+#include "Scenes/EventLog.h"
+#include <gtest/gtest.h>
+
+using namespace Scenes;
+
+class EventLogTests : public ::testing::Test
+{
+protected:
+	size_t linesRead = 0;
+	EventLog log = EventLog(linesRead);
+};
+
+TEST_F(EventLogTests, AddAndQueryLog)
+{
+	LogNameType testStr1 = "Test Event 1";
+	LogResultType expected;
+
+	EXPECT_EQ(expected, log.queryLog(testStr1));
+
+	log.addLog(testStr1);
+	expected.push_back(linesRead);
+	linesRead++;
+	log.addLog(testStr1);
+	expected.push_back(linesRead);
+
+	EXPECT_EQ(expected, log.queryLog(testStr1));
+}
