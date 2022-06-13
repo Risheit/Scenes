d1bad1efbc1cdef251c82cc0c7999c34d33ee3a0
Risheit <80658104+Risheit@users.noreply.github.com>
2022-06-13 01:12:20 -0500

    fixed eventLog and added tests

--- tests/UnitTests/EventTests.cpp
+++ tests/UnitTests/EventTests.cpp
@@ -1,2 +1,49 @@
 #include "Scenes/Event.hpp"
+#include <gtest/gtest.h>
+#include <string>
 
+using namespace Scenes;
+
+// Demonstrate some basic assertions.
+class EventTests : public ::testing::Test
+{
+protected:
+	size_t linesRead{ 0 };
+	EventLog log{ EventLog(linesRead) };
+	Event<std::string(int)> eve{ Event<std::string(int)>(
+		[](int a) -> std::string { return std::to_string(a); }, "test", log
+		) };
+	Event<int(void)> noargs{ Event<int(void)>(
+		[]() -> int { return 0; }, "test", log
+		) };
+};
+
+TEST_F(EventTests, CallsCorrectly)
+{
+	EXPECT_EQ(std::string("4"), eve(4));
+	EXPECT_EQ(std::string("123499"), eve(123499));
+	EXPECT_EQ(0, noargs());
+}
+
+TEST_F(EventTests, CallsLoggedCorrectly)
+{
+	LogResultType eveCallExpected = {};
+	LogResultType noargsCallExpected = {};
+
+	linesRead++;
+	(void)eve(6);
+	eveCallExpected.push_back(linesRead);
+	linesRead++;
+	linesRead++;
+	(void)eve(6);
+	eveCallExpected.push_back(linesRead);
+
+	EXPECT_EQ(eveCallExpected, log.queryLog(eve.eventString()));
+	EXPECT_EQ(noargsCallExpected, log.queryLog(noargs.eventString()));
+
+	linesRead += 5;
+	noargsCallExpected.push_back(linesRead);
+	(void)noargs();
+
+	EXPECT_EQ(noargsCallExpected, log.queryLog(noargs.eventString()));
+}
