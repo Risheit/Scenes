0bddcef1140ebcd7416becd15891db3ba4c034b5
Risheit <80658104+Risheit@users.noreply.github.com>
2022-06-19 17:32:42 -0500

    Create template specialization so Event works with void functions.

--- include/Scenes/Event.hpp
+++ include/Scenes/Event.hpp
@@ -48,6 +48,7 @@ namespace Scenes
 		EventLog& eventLogRef)
 		: _function(func), _name(name), _returnValue(Ret()), _eventLogRef(eventLogRef)
 	{
+		
 		if (name.find(",") != std::string::npos)
 			throw std::invalid_argument{ "Event name cannot contain ','." };
 	}
@@ -78,5 +79,76 @@ namespace Scenes
 		_returnValue = _function(args...);
 		_eventLogRef.addLog(eventString());
 		return _returnValue;
+	}
+
+
+
+
+	// Specialize Event for void functions, as void _returnValue cannot exist
+	template<class ...Args>
+	class Event<void(Args...)>
+	{
+	public:
+		Event(
+			std::function<void(Args...)> func,
+			std::string name,
+			EventLog& eventLogRef
+		);
+
+		virtual void operator()(
+			Args... args
+			);
+
+		virtual std::string eventString() const;
+
+		const std::type_info& targetType() const;
+
+		const std::string& name() const;
+
+	protected:
+		const std::function<void(Args...)> _function;
+		const std::string _name;
+		EventLog& _eventLogRef;
+	};
+
+	/// <typeparam name="...Args">Arguments.</typeparam>
+	/// <param name="func">Contained functor.</param>
+	/// <param name="name">The name of the event. Cannot contain ','.</param>
+	/// <param name="eventLogRef">Refernce to log to store event calls in.</param>
+	template<typename ...Args>
+	inline Event<void(Args...)>::Event(std::function<void(Args...)> func, std::string name,
+		EventLog& eventLogRef)
+		: _function(func), _name(name), _eventLogRef(eventLogRef)
+	{
+
+		if (name.find(",") != std::string::npos)
+			throw std::invalid_argument{ "Event name cannot contain ','." };
+	}
+
+	template<class ...Args>
+	inline std::string Event<void(Args...)>::eventString() const
+	{
+		std::stringstream ss;
+		ss << _name << ",";
+		return ss.str();
+	}
+
+	template<class ...Args>
+	inline const std::type_info& Event<void(Args...)>::targetType() const
+	{
+		return _function.target_type();
+	}
+
+	template<class ...Args>
+	inline const std::string& Event<void(Args...)>::name() const
+	{
+		return _name;
+	}
+
+	template<class ...Args>
+	inline void Event<void(Args...)>::operator()(Args ...args)
+	{
+		_function(args...);
+		_eventLogRef.addLog(eventString());
 	}
 }
--- tests/unittests/EventTests.cpp
+++ tests/unittests/EventTests.cpp
@@ -10,12 +10,9 @@ class EventTests : public ::testing::Test
 protected:
 	size_t linesRead{ 0 };
 	EventLog log{ EventLog(linesRead) };
-	Event<std::string(int)> eve{ Event<std::string(int)>(
-		[](int a) -> std::string { return std::to_string(a); }, "test", log
-		) };
-	Event<int(void)> noargs{ Event<int(void)>(
-		[]() -> int { return 0; }, "test", log
-		) };
+	Event<std::string(int)> eve{ [](int a) -> std::string { return std::to_string(a); }, "test", log };
+	Event<int(void)> noargs{ []() -> int { return 0; }, "test", log };
+	Event<void(int)> noreturn{ [](int a) { std::cout << a; }, "test", log };
 };
 
 TEST_F(EventTests, CallsCorrectly)
@@ -23,13 +20,13 @@ TEST_F(EventTests, CallsCorrectly)
 	EXPECT_EQ(std::string("4"), eve(4));
 	EXPECT_EQ(std::string("123499"), eve(123499));
 	EXPECT_EQ(0, noargs());
+	noreturn(4);
 }
 
 TEST_F(EventTests, CallsLoggedCorrectly)
 {
-	LogResultType eveCallExpected = {};
-	LogResultType noargsCallExpected = {};
 
+	LogResultType eveCallExpected;
 	linesRead++;
 	(void)eve(6);
 	eveCallExpected.push_back(linesRead);
@@ -37,13 +34,23 @@ TEST_F(EventTests, CallsLoggedCorrectly)
 	linesRead++;
 	(void)eve(6);
 	eveCallExpected.push_back(linesRead);
-
 	EXPECT_EQ(eveCallExpected, log.query(eve.eventString()));
+	
+	
+	LogResultType noargsCallExpected;
 	EXPECT_EQ(noargsCallExpected, log.query(noargs.eventString()));
 
 	linesRead += 5;
 	noargsCallExpected.push_back(linesRead);
 	(void)noargs();
-
 	EXPECT_EQ(noargsCallExpected, log.query(noargs.eventString()));
+
+
+	LogResultType noreturnCallExpected;
+	noreturnCallExpected.push_back(linesRead);
+	noreturn(5);
+	linesRead++;
+	noreturnCallExpected.push_back(linesRead);
+	noreturn(8);
+	EXPECT_EQ(noreturnCallExpected, log.query(noreturn.eventString()));
 }
