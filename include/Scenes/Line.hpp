bd9fc8743e2b493e1c8728499d47df94e88f6a33
Risheit <80658104+Risheit@users.noreply.github.com>
2022-06-25 17:12:31 -0500

    added line class.

--- include/Scenes/Line.hpp
+++ include/Scenes/Line.hpp
@@ -1,11 +1,32 @@
 #pragma once
+#include <string>
+#include <optional>
+
+#include "Event.hpp"
 
 namespace Scenes
 {
-
     class Line
     {
+    public:
+        Line(
+            std::string text,
+            Event <std::string> event
+        );
 
+        explicit Line(
+            std::string text
+        );
+
+        void readLine(
+            std::ostream& stream,
+            std::string eventArgs = ""
+        );
+
+    public:
+        const std::string text;
+
+    private:
+        std::optional<Event<std::string> > _event;
     };
-
 } // Scenes
--- src/Line.cpp
+++ src/Line.cpp
@@ -1,6 +1,26 @@
 #include "Line.hpp"
 
+#include <string>
+#include <utility>
+
+#include "Event.hpp"
+
 namespace Scenes
 {
 
+    Line::Line(std::string text, Event<std::string> event)
+        : text(std::move(text)),
+          _event(std::make_optional<Event<std::string> >(std::move(event)))
+    {}
+
+    Line::Line(std::string text)
+        : text(std::move(text))
+    {}
+
+    void Line::readLine(std::ostream& stream, std::string eventArg)
+    {
+        stream << text;
+        if (_event.has_value())
+            (*_event)(std::move(eventArg));
+    }
 } // Scenes
