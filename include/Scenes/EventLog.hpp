c9eeccb9a0cdd384d10716492f5afa6c872d91fa
Risheit <80658104+Risheit@users.noreply.github.com>
2022-07-05 11:52:27 -0500

    Add doxygen comments for EventLog.hpp

--- include/Scenes/EventLog.hpp
+++ include/Scenes/EventLog.hpp
@@ -1,16 +1,38 @@
+/**
+ * @file EventLog.hpp
+ * @brief Contains the EventLog class.
+ */
+
 #pragma once
 
 #include "Log.hpp"
 
 namespace Scenes
 {
+    /**
+     * @brief A Log class that stores records of Events that have been run.
+     *
+     * Event records names are of the form "Event name,return value" and are called eventStrings.
+     *
+     * @copydetails Scenes::Log
+     */
 	class EventLog final : public Log
 	{
 	public:
+        /**
+         * @brief Initializes a new instance of the EventLog class.
+         *
+         * @copydetails Scenes::Log::Log(const size_t&)
+         */
 		explicit EventLog(
 			const size_t& linesRead
 		);
 
+        /**
+         * @brief Finds what eventStrings in this log contain a given Event name.
+         * @param eventName The name to search for.
+         * @return A vector of all the eventStrings that contain eventName, or an empty vector if none were found.
+         */
 		[[nodiscard]] std::vector<LogNameType> findKeys(
 			const LogNameType& eventName
 		) const override;
