0ced1968d366806aa543c8e4e611a5f2f9fcfdb4
Risheit <80658104+Risheit@users.noreply.github.com>
2022-07-05 11:30:20 -0500

    Add doxygen comments to Log.hpp

--- include/Scenes/Log.hpp
+++ include/Scenes/Log.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file Log.cpp
+ * @brief Contains the Log class along with relevant types and functions.
+ */
+
 #pragma once
 #include <string>
 #include <unordered_map>
@@ -5,10 +10,19 @@
 
 namespace Scenes
 {
-	using LogNameType = std::string;
-	using LogResultType = std::vector<size_t>;
-	using LogType = std::unordered_map<LogNameType, LogResultType>;
+	using LogNameType = std::string; //!< The type of key that Log stores.
+	using LogResultType = std::vector<size_t>; //!< The type of result Log stores.
+	using LogType = std::unordered_map<LogNameType, LogResultType>; //!< The type of every record in a Log.
 
+    /**
+     * @brief Contains a record of key names and a vector of the times they were called.
+     *
+     * A Log class implements a hash table with string keys and values comprised of a vector of the line number at
+     * which the record was logged. Log provides methods to add new records, query for a
+     * specific record, and find records that contain a substring. As adding to a Log isn't meant to be done manually,
+     * no tools are provided to delete added records.
+     *
+     */
 	class Log
 	{
 	public:
@@ -18,26 +32,56 @@ namespace Scenes
 		Log& operator=(const Log&&) = delete;
 		virtual ~Log() = default;
 
+        /**
+         * @brief Instantiates a new instance of the Log class.
+         *
+         * @param linesRead A reference to the current line number in a game.
+         */
 		explicit Log(
 			const size_t& linesRead
 		);
 
+        /**
+         * @brief Adds a new record or updates the result of an existing record in this Log.
+         *
+         * When adding to a Log, if a name already exists, the current line number is added to the end of the name's
+         * result vector. If the name doesn't exist in the log, a new name is created and the current line number is
+         * added to a new result vector.
+         * @param name The name of the record to add or update.
+         */
 		virtual void addLog(
 			const LogNameType& name
 		);
 
+        /**
+         * @brief Queries this Log for the result vector of a provided record name.
+         *
+         * @param name The name of the record to query for.
+         * @return A vector containing line numbers of when this record was logged, or an empty vector if the query
+         * fails.
+         */
 		[[nodiscard]] virtual LogResultType query(
 			const LogNameType& name
 		) const;
 
+        /**
+         * @brief Finds what record names in this Log contain a given search term.
+         *
+         * @param searchTerm The substring to search for.
+         * @return A vector of all the names that contain searchTerm, or an empty vector if no names were found.
+         */
 		[[nodiscard]] virtual std::vector<LogNameType> findKeys(
 			const LogNameType& searchTerm
 		) const;
 
+        /**
+         * @brief Checks if this Log is empty (it contains no records).
+         *
+         * @return True if this Log is empty, false otherwise.
+         */
         [[nodiscard]] bool empty() const noexcept;
 	protected:
-		// Map ordered with EventString -> [how many lines passed each call]
-		LogType _log;
-		const size_t& _linesRead;
+		LogType _log; //!< The hash table that stores all records in this Log.
+		const size_t& _linesRead; //!< A reference to the current number of lines that have passed.
 	};
 } // Scenes
